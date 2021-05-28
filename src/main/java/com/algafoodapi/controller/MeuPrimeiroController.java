ff381911eab032b261e4a29da96a745ced03a186
Alexandre Zanlorenzi <android.duim@gmail.com>
2021-05-28 14:16:22 -0300

    start project

---
+++ src/main/java/com/algafoodapi/controller/MeuPrimeiroController.java
@@ -0,0 +1,15 @@
+package com.algafoodapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
+public class MeuPrimeiroController {
+
+  @GetMapping
+  public String hello() {
+    return "hello";
+  }
+}
