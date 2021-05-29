91ea31f6a76f5627d5861a45feb4736a46c14068
Alexandre Zanlorenzi <android.duim@gmail.com>
2021-05-29 19:59:05 -0300

    @jsonproperty / xml

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,10 +1,12 @@
 package com.algaworks.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
-
+//JsonRootName("cozinha") somente xml.
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -15,6 +17,9 @@ public class Cozinha {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+//  @JsonIgnore
+//  @JsonProperty("titulo")
   @Column (nullable = false)
   private String nome;
 }
+
