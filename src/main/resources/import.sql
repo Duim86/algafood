b9ae3050030ab275f19f81cf1d0e24b1d120c9aa
Alexandre Zanlorenzi <android.duim@gmail.com>
2021-05-29 19:07:27 -0300

    @RequestMapping @GetMapping

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,6 @@
 INSERT INTO cozinha (id, nome) values (1, 'Tailandesa');
 INSERT INTO cozinha (id, nome) values (2, 'Indiana');
+INSERT INTO cozinha (id, nome) values (3, 'Japonesa');
 
 INSERT INTO restaurante (nome, taxa_frete, cozinha_id) values ('Thai Food', 7.25, 1);
 INSERT INTO restaurante (nome, taxa_frete, cozinha_id) values ('Indian Food', 10.5, 2);
